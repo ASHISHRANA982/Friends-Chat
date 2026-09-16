d5ce89c578d1d85136d5a95d8f7028b05977b0a3
ASHISHRANA982 <ashishranaansi1234@gmail.com>
2026-09-16 22:54:29 +0530

    pwa added successful

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,10 +1,45 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-   define: {
+  define: {
     global: 'globalThis',
   },
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "Friends Chat",
+        short_name: "Friends Chat",
+        description:
+          "Instantly send and receive messages in real-time with your friends.",
+
+        theme_color: "#111B21",
+        background_color: "#111B21",
+
+        display: "standalone",
+        start_url: "/",
+        scope: "/",
+        id: "/",
+
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+        ],
+      },
+    })
+  ],
+});
